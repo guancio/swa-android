0fddf5b80483e50908c89ec39241c74c360e82e5
Roberto Guanciale <robertog@kth.se>
2017-03-16 17:17:35 +0100

    Small app to use the camera

---
+++ Camera/main/java/se/kth/swaccademy/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package se.kth.swaccademy.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void click(View p) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView mImageView = (ImageView) findViewById(R.id.img);
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
